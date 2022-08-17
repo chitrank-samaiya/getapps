adb1ff5efb9b8e226915018b530c34c183e4f50e
Chitrank Samaiya <Chitrank_Samaiya@external.mckinsey.com>
2022-08-17 22:08:02 +0530

    0. Created parser capterra.rb inside lib folder

---
+++ lib/parser/capterra.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+
+module Parser
+  class Capterra
+
+    def self.parse_and_transform(filepath)
+      YAML.load_file(filepath).map do |_hash|
+        {}.tap do |_inner_hash|
+          _inner_hash["Name"] = _hash["name"]
+          _inner_hash["Categories"] = _hash["tags"]
+          _inner_hash["Twitter"] = _hash["twitter"]
+        end
+      end
+    end
+  end
+end
