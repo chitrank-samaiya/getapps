0f9a483c00e027221bb237436c76f7273cd495df
Chitrank Samaiya <Chitrank_Samaiya@external.mckinsey.com>
2022-08-17 22:12:15 +0530

    0. Created parser softwareadvice.rb inside lib folder for json parsing

---
+++ lib/parser/softwareadvice.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+module Parser
+  class Softwareadvice
+
+    def self.parse_and_transform(filepath)
+      JSON.parse(File.read(filepath))["products"].map do |_hash|
+        {}.tap do |_inner_hash|
+          _inner_hash["Name"] = _hash["title"]
+          _inner_hash["Categories"] = _hash.fetch("categories", []).join(', ')
+          _inner_hash["Twitter"] = _hash["twitter"]
+        end
+      end
+    end
+  end
+end
